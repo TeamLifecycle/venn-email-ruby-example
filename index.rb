f2301c584fb7068c5a82bfba04240b877dd5b8cb
Tim Giblin <timmyg13@gmail.com>
2015-04-08 20:37:04 -0400

    fix imports

--- index.rb
+++ index.rb
@@ -1,4 +1,15 @@
 require 'venn/email_client'
+require 'multi_json'
+require 'rest-client'
+require 'mandrill'
+require 'sendgrid-ruby'
 
-client = Venn::EmailClient.new '6644bb87bcd7fceb2cd53436'
-client.send 'from@email.com', 'timmyg13@gmail.com', 'subject_line ruby', 'message_body'
+require "venn/version"
+require 'venn/email_client'
+require 'venn/services/api'
+require 'venn/services/mandrill'
+require 'venn/services/sendgrid'
+
+client = Venn::EmailClient.new 'VENN API KEY HERE'
+result = client.send 'from@email.com', 'TO: EMAIL ADDRESS HERE', 'subject_line ruby', 'message_body'
+puts 'successfully sent with: ' + result
